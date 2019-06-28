11ddac08904945986dfe87e33613d192c09457b3
mehmet.ozdemir <mehmet.ozdemir@IT_MBL_09.tarsim.org.tr>
2019-06-28 11:13:50 +0300

    First Commit

---
+++ GitProjectDeneme/src/FirstClass.java
@@ -0,0 +1,31 @@
+
+public class FirstClass {
+
+	private String tc;
+	private String name;
+	private String surname;
+
+	public String getTc() {
+		return tc;
+	}
+
+	public void setTc(String tc) {
+		this.tc = tc;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+}
